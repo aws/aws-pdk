f3c66bd27531cc69b0a434c6eac9fa34ac5d43d7
Adrian Dimech <dimecha@amazon.com>
2022-05-13 14:06:47 +1000

    fix: revert back to correct exports

--- packages/aws-prototyping-sdk/src/index.ts
+++ packages/aws-prototyping-sdk/src/index.ts
@@ -1,8 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export class Hello {
-  public sayHello() {
-    return "hello, world!";
-  }
-}
+export * as pdk_pipeline from "./pdk_pipeline";
+export * as pdk_projen from "./pdk_projen";
