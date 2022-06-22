c607a4341e3973c53a0623fbdde275380525b0be
Adrian Dimech <dimecha@amazon.com>
2022-06-22 15:19:21 +1000

    fix: remove RuntimeConfig helper due to inter-dep issue

--- packages/static-website/src/static-website.ts
+++ packages/static-website/src/static-website.ts
@@ -13,8 +13,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  ******************************************************************************************************************** */
-import { UserIdentity } from "@aws-prototyping-sdk/identity"; // eslint-disable-line import/no-extraneous-dependencies
-import { CfnResource, RemovalPolicy, Stack } from "aws-cdk-lib";
+import { RemovalPolicy } from "aws-cdk-lib";
 import {
   Distribution,
   DistributionProps,
@@ -24,7 +23,6 @@ import {
   OriginBindOptions,
 } from "aws-cdk-lib/aws-cloudfront";
 import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
-import { CfnUserPoolClient } from "aws-cdk-lib/aws-cognito";
 import { Key } from "aws-cdk-lib/aws-kms";
 import { Bucket, BucketEncryption, IBucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
@@ -60,51 +58,6 @@ export interface RuntimeOptions {
    * @example { userPoolId: some.userPool.userPoolId, someResourceArn: some.resource.Arn }
    */
   readonly jsonPayload: any;
-}
-
-/**
- * Helper functions for generating runtime config payloads.
- */
-export class RuntimeOptions {
-  /**
-   * Generates a runtime config payload based on the UserIdentity.
-   *
-   * If the UserIdentity did not create the UserPool, this function will attempt to locate the first
-   * userPoolClient attached to the UserPool. If a UserPoolClient cannot be found, an Error is thrown.
-   *
-   * @param scope CDK scope.
-   * @param props UserIdentity instance.
-   * @returns A runtime-config payload.
-   */
-  public static fromUserIdentity(
-    scope: Construct,
-    { userPool, userPoolClient, identityPool }: UserIdentity
-  ) {
-    let region = Stack.of(scope).region;
-    let identityPoolId = identityPool.identityPoolId;
-    let userPoolId = userPool.userPoolId;
-    let userPoolWebClientId = userPoolClient?.userPoolClientId;
-
-    if (!userPoolWebClientId) {
-      const cfnUserPoolClient = userPool.node.children.find(
-        (c) =>
-          (c.node.defaultChild as CfnResource)?.cfnResourceType ===
-          "AWS::Cognito::UserPoolClient"
-      )?.node.defaultChild;
-      userPoolWebClientId = cfnUserPoolClient
-        ? (cfnUserPoolClient as CfnUserPoolClient).ref
-        : (() => {
-            throw Error("Cannot determine the userPoolWebClientId.");
-          })();
-    }
-
-    return {
-      region,
-      userPoolId,
-      userPoolWebClientId,
-      identityPoolId,
-    };
-  }
 }
 
 /**
