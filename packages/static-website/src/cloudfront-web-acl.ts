2bd5d61452faaba546b09346b1a9603c726e66da
Dimech <dimecha@bcd07403f081.ant.amazon.com>
2022-07-12 10:40:35 +1000

    fix: improve security posture of static-website

--- packages/static-website/src/cloudfront-web-acl.ts
+++ packages/static-website/src/cloudfront-web-acl.ts
@@ -14,10 +14,17 @@
  limitations under the License.
  ******************************************************************************************************************** */
 import * as path from "path";
-import { CustomResource, Duration } from "aws-cdk-lib";
-import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { CustomResource, Duration, Stack } from "aws-cdk-lib";
+import {
+  Effect,
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Provider } from "aws-cdk-lib/custom-resources";
+import { NagSuppressions } from "cdk-nag";
 import { Construct } from "constructs";
 
 /**
@@ -93,6 +100,80 @@ export class CloudfrontWebAcl extends Construct {
   constructor(scope: Construct, id: string, props?: CloudFrontWebAclProps) {
     super(scope, id);
 
+    const stack = Stack.of(this);
+    const aclName = `${stack.stackName}-${id}`; // Unique per stack
+    const onEventHandler = this.createOnEventHandler(stack, aclName);
+    const customResource = this.createAclCustomResource(
+      stack,
+      aclName,
+      onEventHandler,
+      props
+    );
+
+    this.webAclId = customResource.getAttString("WebAclId");
+    this.webAclArn = customResource.getAttString("WebAclArn");
+  }
+
+  /**
+   * Creates an event handler for managing an ACL in us-east-1.
+   *
+   * @param stack containing Stack instance.
+   * @param aclName name of the ACL to manage.
+   * @private
+   */
+  private createOnEventHandler(stack: Stack, aclName: string): Function {
+    const onEventHandlerName = `${stack.stackName}-OnEventHandler`;
+    const onEventHandlerRole = new Role(this, "OnEventHandlerRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+      inlinePolicies: {
+        logs: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+              ],
+              resources: [
+                `arn:aws:logs:${stack.region}:${stack.account}:log-group:/aws/lambda/${onEventHandlerName}`,
+                `arn:aws:logs:${stack.region}:${stack.account}:log-group:/aws/lambda/${onEventHandlerName}:*`,
+              ],
+            }),
+          ],
+        }),
+        wafv2: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: [
+                "wafv2:CreateWebACL",
+                "wafv2:DeleteWebACL",
+                "wafv2:UpdateWebACL",
+                "wafv2:GetWebACL",
+              ],
+              resources: [
+                `arn:aws:wafv2:us-east-1:${stack.account}:global/webacl/${aclName}/*`,
+                `arn:aws:wafv2:us-east-1:${stack.account}:global/managedruleset/*/*`,
+              ],
+            }),
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: [
+                "wafv2:CreateIPSet",
+                "wafv2:DeleteIPSet",
+                "wafv2:UpdateIPSet",
+                "wafv2:GetIPSet",
+              ],
+              resources: [
+                `arn:aws:wafv2:us-east-1:${stack.account}:global/ipset/${aclName}-IPSet/*`,
+              ],
+            }),
+          ],
+        }),
+      },
+    });
+
     const onEventHandler = new Function(
       this,
       "CloudfrontWebAclOnEventHandler",
@@ -100,46 +181,120 @@ export class CloudfrontWebAcl extends Construct {
         code: Code.fromAsset(
           path.join(__dirname, "../lib/webacl_event_handler")
         ),
-        functionName: "CloudfrontWebAclCustomResource",
+        role: onEventHandlerRole,
+        functionName: onEventHandlerName,
         handler: "index.onEvent",
         runtime: Runtime.NODEJS_16_X,
         timeout: Duration.seconds(300),
       }
     );
 
-    onEventHandler.role!.addToPrincipalPolicy(
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: [
-          "wafv2:CreateWebACL",
-          "wafv2:DeleteWebACL",
-          "wafv2:UpdateWebACL",
-          "wafv2:GetWebACL",
-          "wafv2:CreateIPSet",
-          "wafv2:DeleteIPSet",
-          "wafv2:UpdateIPSet",
-          "wafv2:GetIPSet",
-        ],
-        resources: ["*"],
-      })
+    NagSuppressions.addResourceSuppressions(
+      onEventHandlerRole,
+      [
+        {
+          id: "AwsSolutions-IAM5",
+          reason:
+            "WafV2 resources have been scoped down to the ACL/IPSet level, however * is still needed as resource id's are created just in time.",
+          appliesTo: [
+            {
+              regex:
+                "/^Resource::arn:aws:wafv2:us-east-1:<AWS::AccountId>:global/(.*)$/g",
+            },
+          ],
+        },
+        {
+          id: "AwsSolutions-IAM5",
+          reason:
+            "Cloudwatch resources have been scoped down to the LogGroup level, however * is still needed as stream names are created just in time.",
+          appliesTo: [
+            {
+              regex: `/^Resource::arn:aws:logs:<AWS::Region>:<AWS::AccountId>:log-group:/aws/lambda/${onEventHandlerName}:\*/g`,
+            },
+          ],
+        },
+      ],
+      true
     );
 
+    return onEventHandler;
+  }
+
+  /**
+   * Creates a Custom resource to manage the deployment of the ACL.
+   *
+   * @param stack containing Stack instance.
+   * @param aclName name of the ACL to manage.
+   * @param onEventHandler event handler to use for deployment.
+   * @param props user provided properties for configuring the ACL.
+   * @private
+   */
+  private createAclCustomResource(
+    stack: Stack,
+    aclName: string,
+    onEventHandler: Function,
+    props?: CloudFrontWebAclProps
+  ): CustomResource {
+    const providerFunctionName = `${onEventHandler.functionName}-Provider`;
+    const providerRole = new Role(this, "CloudfrontWebAclProviderRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+      inlinePolicies: {
+        logs: new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+              ],
+              resources: [
+                `arn:aws:logs:${stack.region}:${stack.account}:log-group:/aws/lambda/${providerFunctionName}`,
+                `arn:aws:logs:${stack.region}:${stack.account}:log-group:/aws/lambda/${providerFunctionName}:*`,
+              ],
+            }),
+          ],
+        }),
+      },
+    });
     const provider = new Provider(this, "CloudfrontWebAclProvider", {
       onEventHandler,
+      role: providerRole,
+      providerFunctionName,
     });
 
-    const customResource = new CustomResource(this, "CFWebAclCustomResource", {
+    NagSuppressions.addResourceSuppressions(
+      providerRole,
+      [
+        {
+          id: "AwsSolutions-IAM5",
+          reason:
+            "Cloudwatch resources have been scoped down to the LogGroup level, however * is still needed as stream names are created just in time.",
+        },
+      ],
+      true
+    );
+    NagSuppressions.addResourceSuppressions(
+      provider,
+      [
+        {
+          id: "AwsSolutions-L1",
+          reason:
+            "Latest runtime cannot be configured. CDK will need to upgrade the Provider construct accordingly.",
+        },
+      ],
+      true
+    );
+
+    return new CustomResource(this, "CFWebAclCustomResource", {
       serviceToken: provider.serviceToken,
       properties: {
-        ID: id,
+        ID: aclName,
         MANAGED_RULES: props?.managedRules ?? [
           { vendor: "AWS", name: "AWSManagedRulesCommonRuleSet" },
         ],
         CIDR_ALLOW_LIST: props?.cidrAllowList,
       },
     });
-
-    this.webAclId = customResource.getAttString("WebAclId");
-    this.webAclArn = customResource.getAttString("WebAclArn");
   }
 }
