35082e069c0ca8714a9f8b91a997bf2710a38177
Adrian Dimech <51220968+agdimech@users.noreply.github.com>
2022-07-15 13:05:13 +1000

    fix: ensure aspects run for Pipeline stages (#94)

--- packages/pipeline/test/pdk-pipeline.test.ts
+++ packages/pipeline/test/pdk-pipeline.test.ts
@@ -18,6 +18,7 @@ import * as path from "path";
 import { PDKNag } from "@aws-prototyping-sdk/pdk-nag";
 import { Stack, Stage } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { Asset } from "aws-cdk-lib/aws-s3-assets";
 import { PDKPipeline } from "../src";
 
@@ -48,5 +49,35 @@ describe("PDK Pipeline Unit Tests", () => {
     pipeline.buildPipeline();
 
     expect(Template.fromStack(stack)).toMatchSnapshot();
+  });
+
+  it("StageNagRuns", () => {
+    const app = PDKNag.app({ failOnError: false });
+    const stack = new Stack(app);
+
+    const pipeline = new PDKPipeline(stack, "StageNagRuns", {
+      primarySynthDirectory: "cdk.out",
+      repositoryName: "StageNagRuns",
+      synth: {},
+      sonarCodeScannerConfig: {
+        sonarqubeAuthorizedGroup: "dev",
+        sonarqubeDefaultProfileOrGateName: "dev",
+        sonarqubeEndpoint: "https://sonar.dev",
+        sonarqubeProjectName: "Default",
+      },
+    });
+
+    const stage = new Stage(app, "Stage");
+    const appStack = new Stack(stage, "AppStack");
+    new Bucket(appStack, "Non-Compliant");
+
+    pipeline.addStage(stage);
+    pipeline.buildPipeline();
+
+    app.synth();
+
+    expect(app.nagResults()[0].resource).toEqual(
+      "Stage/AppStack/Non-Compliant/Resource"
+    );
   });
 });
