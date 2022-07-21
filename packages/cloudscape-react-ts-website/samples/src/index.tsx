3d4d93e9a40813fbea947b2607103db37f043025
Dimech <dimecha@bcd07403f081.ant.amazon.com>
2022-07-21 19:09:36 +1000

    fix: refactor Cloudscape sample to support sign out dropdown

--- packages/cloudscape-react-ts-website/samples/src/index.tsx
+++ packages/cloudscape-react-ts-website/samples/src/index.tsx
@@ -16,9 +16,7 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import {
-  BrowserRouter,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@cloudscape-design/global-styles/index.css';
