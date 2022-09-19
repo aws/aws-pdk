f094ac36262a7a4add76bea78f93eefef6748afe
🚀 Jack <jacsteve@amazon.com>
2022-09-19 14:58:05 +1000

    feat(open-api-gateway): prefer user-specified dependency versions for auto-added dependencies
    Where a user has specified a dependency version, we prefer that over the ones we add automatically.
    This allows for pinning of the @aws-prototyping-sdk/open-api-gateway package version, as well as
    cdk, constructs etc.
    
    fix #164

--- packages/open-api-gateway/test/project/open-api-gateway-java-project/standalone.test.ts
+++ packages/open-api-gateway/test/project/open-api-gateway-java-project/standalone.test.ts
@@ -31,4 +31,31 @@ describe("OpenAPI Gateway Java Standalone Unit Tests", () => {
     });
     expect(synthSnapshot(project)).toMatchSnapshot();
   });
+
+  it("Honours Dependency Versions", () => {
+    const project = new OpenApiGatewayJavaProject({
+      name: "myapi",
+      groupId: "software.aws.test",
+      artifactId: "my-api",
+      version: "1.0.0",
+      clientLanguages: [],
+      deps: [
+        "software.aws.awsprototypingsdk/open-api-gateway@0.10.2",
+        "software.constructs/constructs@10.1.7",
+        "software.amazon.awscdk/aws-cdk-lib@2.39.0",
+      ],
+    });
+
+    expect(
+      project.deps.getDependency(
+        "software.aws.awsprototypingsdk/open-api-gateway"
+      ).version
+    ).toBe("0.10.2");
+    expect(
+      project.deps.getDependency("software.constructs/constructs").version
+    ).toBe("10.1.7");
+    expect(
+      project.deps.getDependency("software.amazon.awscdk/aws-cdk-lib").version
+    ).toBe("2.39.0");
+  });
 });
