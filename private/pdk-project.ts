4b3b7ae1fe716e969010a2aca6ac90089b16c2c1
Adrian Dimech <51220968+agdimech@users.noreply.github.com>
2022-11-14 12:47:56 +1100

    fix: change license header format to SPDX-License-Identifier (#205)
    fix #199
    
    Co-authored-by: Dimech <dimecha@bcd07403f081.ant.amazon.com>

--- private/pdk-project.ts
+++ private/pdk-project.ts
@@ -1,18 +1,5 @@
-/*********************************************************************************************************************
- Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License").
- You may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- ******************************************************************************************************************** */
+/*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0 */
 
 import { SampleDir } from "projen";
 import {
@@ -138,7 +125,7 @@ export class PDKProject extends JsiiProject {
       description: "Run eslint against the staged files only",
       steps: [
         {
-          exec: "eslint --fix --no-error-on-unmatched-pattern $(git diff --name-only --relative --staged HEAD . | grep -E '.(ts|tsx)$' | xargs)",
+          exec: "eslint --fix --no-error-on-unmatched-pattern $(git diff --name-only --relative --staged HEAD . | grep -E '.(ts|tsx)$' | grep -v 'samples/*' | xargs)",
         },
       ],
     });
