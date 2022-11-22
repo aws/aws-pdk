6704e6fb942550be8ce76c08ed131a9596575d20
dboyd13 <dboyd13@gmail.com>
2022-11-22 11:53:41 +1100

    feat(nagpack): foundations for Prototyping NagPack (#213)
    * feat(nagpack): foundations for Prototyping NagPack
    
    * feat(nagpack): added missing jsdocs
    
    * feat(feat/pdk-nagpack-awsprototypingchecks): feat/PDK-NagPack-AwsPrototypingChecks
    
    Co-authored-by: Boyd <drboyd@88665a27d7e4.ant.amazon.com>

---
+++ packages/pdk-nag/src/packs/aws-prototyping-rules.ts
@@ -0,0 +1,37 @@
+/*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0 */
+import { NagMessageLevel, rules } from "cdk-nag";
+
+/**
+ * Pack name
+ *
+ */
+export let PackName = "AwsPrototyping";
+
+/**
+ * Rule metadata
+ *
+ */
+export let RuleMetadata = [
+  {
+    info: "The S3 bucket does not prohibit public access through bucket level settings.",
+    explanation:
+      "Keep sensitive data safe from unauthorized remote users by preventing public access at the bucket level.",
+    level: NagMessageLevel.ERROR,
+    rule: rules.s3.S3BucketLevelPublicAccessProhibited,
+  },
+  {
+    info: "The S3 Bucket does not have object lock enabled.",
+    explanation:
+      "Because sensitive data can exist at rest in S3 buckets, enforce object locks at rest to help protect that data.",
+    level: NagMessageLevel.WARN,
+    rule: rules.s3.S3BucketDefaultLockEnabled,
+  },
+  {
+    info: "The Lambda Function URL allows for public, unauthenticated access.",
+    explanation:
+      "AWS Lambda Function URLs allow you to invoke your function via a HTTPS end-point, setting the authentication to NONE allows anyone on the internet to invoke your function.",
+    level: NagMessageLevel.ERROR,
+    rule: rules.lambda.LambdaFunctionUrlAuth,
+  },
+];
---
+++ packages/pdk-nag/src/packs/aws-prototyping.ts
@@ -0,0 +1,31 @@
+/*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0 */
+
+import { CfnResource } from "aws-cdk-lib";
+import { NagPack, NagPackProps } from "cdk-nag";
+import { IConstruct } from "constructs";
+import { PackName, RuleMetadata } from "./aws-prototyping-rules";
+
+/**
+ * Check best practices for prototypes
+ *
+ */
+export class AwsPrototypingChecks extends NagPack {
+  constructor(props?: NagPackProps) {
+    super(props);
+    this.packName = PackName;
+  }
+  /**
+   * Check if CfnResource and apply rules
+   */
+  public visit(node: IConstruct): void {
+    if (node instanceof CfnResource) {
+      RuleMetadata.forEach((rule) => {
+        this.applyRule({
+          ...rule,
+          node,
+        });
+      });
+    }
+  }
+}
