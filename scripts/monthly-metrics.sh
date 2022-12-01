487fb51e1c61e3e57bb2d4d93796e97029a283a1
Dimech <dimecha@bcd07403f081.ant.amazon.com>
2022-12-01 15:15:33 +1100

    feat: add PYPI metrics

--- scripts/monthly-metrics.sh
+++ scripts/monthly-metrics.sh
@@ -4,14 +4,22 @@ CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 NO_COLOUR='\033[0m'
 
-TOTAL_COUNT=0;
+TOTAL_NPM_COUNT=0;
+TOTAL_PYPI_COUNT=0;
 printf "`echo $YELLOW`Monthly downloads by Package\n-----------------------------------------------------------\n\n";
 
-for PKG in `npx lerna list`;
+for NPM_PKG in `npx lerna list`;
 do
-  COUNT=`curl -s https://api.npmjs.org/downloads/range/last-month/$PKG | jq '[.downloads[].downloads | tonumber] | add'`;
-  TOTAL_COUNT=$(($TOTAL_COUNT + $COUNT));
-  printf "$CYAN$PKG: $NO_COLOUR$COUNT\n";
+  PKG_DIR=`npx lerna list --scope $NPM_PKG -l | awk '{ print $3 }'`;
+  PYTHON_PKG=`cat $PKG_DIR/package.json | jq -r .jsii.targets.python.distName`;
+
+  NPM_COUNT=`curl -s https://api.npmjs.org/downloads/range/last-month/$NPM_PKG | jq '[.downloads[].downloads | tonumber] | add'`;
+  PYPI_COUNT=`curl -s https://pypistats.org/api/packages/$PYTHON_PKG/recent | jq '.data.last_month'`;
+
+  TOTAL_NPM_COUNT=$(($TOTAL_NPM_COUNT + $NPM_COUNT));
+  TOTAL_PYPI_COUNT=$(($TOTAL_PYPI_COUNT + $PYPI_COUNT));
+
+  printf "$CYAN$NPM_PKG: $NO_COLOUR$NPM_COUNT (NPM), $PYPI_COUNT (PYPI)\n";
 done
 
-printf "$GREEN\nTOTAL: $NO_COLOUR$TOTAL_COUNT\n";
+printf "$GREEN\nTOTAL: $NO_COLOUR$TOTAL_NPM_COUNT (NPM), $TOTAL_PYPI_COUNT (PYPI) \n";
