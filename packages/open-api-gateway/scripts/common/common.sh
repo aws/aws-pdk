9705d3e97eedec75ed6840ac41352ed12c766d01
Gergely Varga <gergelyv@amazon.com>
2023-02-14 11:06:10 +0800

    fix(open-api-gateway): pkg manager support in scripts and debug option

--- packages/open-api-gateway/scripts/common/common.sh
+++ packages/open-api-gateway/scripts/common/common.sh
@@ -2,43 +2,46 @@
 
 set -e
 
-# Determine which package manager we're using
-# Preference: pnpm > yarn > npm
-# Use if available, otherwise fall back to npm so as not to require it as a system-wide dependency
-pkg_manager=pnpm
-pkg_install_cmd=install
-if ! $pkg_manager -v &> /dev/null; then
-  pkg_manager=yarn
-  pkg_install_cmd=add
-  if ! $pkg_manager -v &> /dev/null; then
-    pkg_manager=npm
-    pkg_install_cmd=install
-  fi
-fi
-
-# yarn classic vs v2+ have different behavior
-# check if it's the classic or later
-if [ "$pkg_manager" == "yarn" ]; then
-  yarn_version=$(yarn --version)
-  yarn_major_version=${yarn_version:0:1}
-fi
-
 ##
-# workaround yarn v2+ needs yarn.lock present for the "add" command
-__prepare_tmpdir() {
-  if [ "$pkg_manager" == "yarn" ]; then
-    if [ "$yarn_major_version" != "1" ]; then
-      touch yarn.lock
-    fi
+# log debug messages if OPEN_API_GATEWAY_DEBUG env var set to 1
+# this is a helper function for debugging our scripts
+log() {
+  if [ "$OPEN_API_GATEWAY_DEBUG" == 1 ]; then
+    echo "$@"
   fi
 }
+
+# Determine which package manager we're using
+# Use pnpm if available, otherwise fall back to npm (it's present if you have node)
+
+# NOTES: removed `yarn` option: yarn v2+ has no backwards compatibility and will
+# fail to run some of the commands because peer dependencies not found;
+# yarn v1 has no significant performance difference from npm/npx
+pkg_manager=pnpm
+if ! $pkg_manager -v &> /dev/null; then
+  pkg_manager=npm
+fi
+
+log "package manager :: $pkg_manager"
 
 ##
 # installs the passed packages with the package manager in use
 install_packages() {
-  __prepare_tmpdir
-  # yarn2's --silent switch doesn't work, hence >/dev/null
-  $pkg_manager $pkg_install_cmd --silent "$@" >/dev/null 2>&1
+  log "installing packages :: $@"
+
+  if [ "$pkg_manager" == "pnpm" ]; then
+    reporter=silent
+    if [ "$OPEN_API_GATEWAY_DEBUG" == 1 ]; then
+      reporter=default
+    fi
+    $pkg_manager install --reporter=$reporter "$@"
+  else
+    silent_switch="--silent"
+    if [ "$OPEN_API_GATEWAY_DEBUG" == 1 ]; then
+      silent_switch=""
+    fi
+    $pkg_manager install $silent_switch "$@"
+  fi
 }
 
 ##
@@ -46,17 +49,14 @@ install_packages() {
 run_command() {
   cmd="$@"
 
-  if [ "$pkg_manager" == "yarn" ]; then
-    if [ "$yarn_major_version" == "1" ]; then
-      runner="yarn run"
-    else
-      runner="yarn dlx"
-    fi
-  elif [ "$pkg_manager" == "pnpm" ]; then
-    runner="pnpx"
+  if [ "$pkg_manager" == "pnpm" ]; then
+    # pnpx cli was deprecated in v6 --> use pnpm dlx
+    runner="pnpm dlx"
   else
     runner="npx"
   fi
+
+  log "running command $runner $cmd"
 
   $runner $cmd >/dev/null 2>&1
 }
