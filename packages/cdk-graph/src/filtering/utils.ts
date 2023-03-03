9dd023d4d9a5fc8d5f68369a41ca2994882ab02d
Jeremy Jonas <jerjonas@amazon.com>
2023-03-03 16:50:06 +0800

    fix(cdk-graph): fix custom resource reference subgraph
    Was picking up resources it should not have, and those were getting pruned

--- packages/cdk-graph/src/filtering/utils.ts
+++ packages/cdk-graph/src/filtering/utils.ts
@@ -3,22 +3,46 @@ SPDX-License-Identifier: Apache-2.0 */
 import { Graph } from "../core";
 
 /** @internal */
-export function findReferencesOfSubGraph(subgraph: Graph.Node): Graph.Node[] {
+export function findReferencesOfSubGraph(
+  subgraph: Graph.Node,
+  depth?: number,
+  predicate?: Graph.INodePredicate,
+  strict: boolean = true
+): Graph.Node[] {
   const chain = new Set<Graph.Node>();
+  if (depth == null) {
+    depth = Number.MAX_SAFE_INTEGER;
+  }
 
-  function follow(_node: Graph.Node) {
+  function follow(_node: Graph.Node, _depth: number) {
+    if (_depth >= depth!) return;
     for (const _ref of _node.references) {
+      if (predicate && !predicate(_ref)) continue;
       if (_ref === subgraph || chain.has(_ref)) continue;
       chain.add(_ref);
-      follow(_ref);
+      follow(_ref, _depth + 1);
     }
 
     for (const child of _node.children) {
-      follow(child);
+      if (predicate && !predicate(child)) continue;
+      follow(child, _depth + 1);
     }
   }
 
-  follow(subgraph);
+  follow(subgraph, 0);
+
+  if (strict) {
+    for (const ref of chain) {
+      if (
+        ref.referencedBy.filter(
+          (_by) =>
+            !(_by === subgraph || _by.isAncestor(subgraph)) && !chain.has(_by)
+        ).length
+      ) {
+        chain.delete(ref);
+      }
+    }
+  }
 
   return Array.from(chain);
 }
