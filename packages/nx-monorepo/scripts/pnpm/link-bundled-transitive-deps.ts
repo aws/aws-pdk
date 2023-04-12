8596eeab896263d3ff6e960e2da064e992746302
Jeremy Jonas <jerjonas@amazon.com>
2023-04-12 08:50:32 +0800

    refactor(nx-monorepo): use `@pnpm/reviewing.dependencies-hierarchy`
    Use `@pnpm/reviewing.dependencies-hierarchy` instead of `pnpm list` to
    get transitive bundled dependencies.

--- packages/nx-monorepo/scripts/pnpm/link-bundled-transitive-deps.ts
+++ packages/nx-monorepo/scripts/pnpm/link-bundled-transitive-deps.ts
@@ -2,63 +2,48 @@
 /*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0 */
 import * as path from "node:path";
-import * as child_process from "node:child_process";
-import { promisify } from "node:util";
 import * as fs from "fs-extra";
 import * as SemVer from "semver";
-
-interface Dependency {
-  readonly from: string;
-  readonly version: string;
-  readonly resolved?: string;
-  readonly path: string;
-  readonly dependencies?: Record<string, Dependency>;
-}
-
-interface PackageTree {
-  readonly name: string;
-  readonly version: string;
-  readonly path: string;
-  readonly private?: boolean;
-  readonly dependencies?: Record<string, Dependency>;
-}
-
-type PnpmListJson = PackageTree[];
+import { buildDependenciesHierarchy, PackageNode } from "@pnpm/reviewing.dependencies-hierarchy";
 
 async function linkBundledTransitiveDeps(workspaceDir: string, pkgFolder: string) {
   const pkgDir = path.join(workspaceDir, pkgFolder);
   const pkgJson = require(path.join(pkgDir, "package.json"));
   const bundledDeps: string[] = pkgJson.bundledDependencies || [];
-  if (bundledDeps.length < 1) {
+  if (!bundledDeps.length) {
     // No bundled deps
     return;
   }
 
-  const listJson: PnpmListJson = JSON.parse((await promisify(child_process.exec)(`pnpm list --depth 10000 --prod --no-optional --json`, { cwd: pkgDir })).stdout);
-  const pkgTree: PackageTree | undefined = listJson.find((v) => v.name === pkgJson.name);
-  if (pkgTree == null || pkgTree.dependencies == null) {
-    throw new Error(`Failed to list pnpm dependencies for package: ${pkgFolder}`);
-  }
+  const dependencyHierarchy = (await buildDependenciesHierarchy([pkgDir], {
+    depth: Number.MAX_SAFE_INTEGER,
+    lockfileDir: workspaceDir,
+    include: {
+      optionalDependencies: false,
+      dependencies: true,
+      devDependencies: false,
+    },
+  }))[pkgDir];
 
-  const transitiveDeps: Record<string, Dependency> = {};
+  const transitiveDeps: Record<string, PackageNode> = {};
 
-  function visit(_deps?: Record<string, Dependency>) {
-    if (_deps == null || !Object.values(_deps).length) {
+  function visit(_deps?: PackageNode[]) {
+    if (_deps == null || !_deps.length) {
       return;
     }
 
-    Object.entries(_deps).forEach(([_key, _dep]) => {
-      if (_dep.resolved == null || _dep.version.startsWith("link:")) {
+    _deps.forEach((_dep) => {
+      if (_dep.resolved == null || _dep.isMissing || _dep.version.startsWith("link:")) {
         // Unresolved / unsaved dependency
         return;
       }
 
       // record the transitive dep with resolved path to symlink
-      const _existing = transitiveDeps[_dep.from];
+      const _existing = transitiveDeps[_dep.alias];
       // Use the latest version of transitive deps only
       // TODO: Can we support multiple versions of transitive deps, and should we?
       if (!_existing || SemVer.gt(_dep.version, _existing.version)) {
-        transitiveDeps[_dep.from] = _dep;
+        transitiveDeps[_dep.alias] = _dep;
 
         // traverse
         visit(_dep.dependencies);
@@ -67,7 +52,7 @@ async function linkBundledTransitiveDeps(workspaceDir: string, pkgFolder: string
   }
 
   for (const _bundledDepName of bundledDeps) {
-    const _bundledDep = pkgTree.dependencies[_bundledDepName] || Object.values(pkgTree.dependencies).find((v) => v.from === _bundledDepName);
+    const _bundledDep = (dependencyHierarchy.dependencies || []).find((v) => v.alias === _bundledDepName);
     if (_bundledDep == null) {
       throw new Error(`Package ${pkgJson.name} bundled dependency "${_bundledDepName}" is missing dependency declaration.`)
     }
