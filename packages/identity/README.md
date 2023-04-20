98e3a08614a58164caa1a32e1f146fa977a65c19
Adrian Dimech <51220968+agdimech@users.noreply.github.com>
2023-04-21 07:48:26 +1000

    feat(identity): support updated MFA props along with a new UserPoolWithMFA construct (#354)
    BREAKING CHANGE: UserPoolWithMfa construct has recently been added which provides a more well
    rounded set of default configurations for the UserPool. It introduces a breaking change in that
    existing customers of the UserIdentity construct prior to v0.15.5 will by default be upgraded to use
    the new defaults which will cause deployments to fail. To workaround this, a new cdk context
    property ‘@aws-prototyping-sdk/identity:useLegacyMFAProps’ was added to revert back to legacy
    defaults.

--- packages/identity/README.md
+++ packages/identity/README.md
@@ -1,3 +1,13 @@
+> **BREAKING CHANGES** (pre-release)
+> - `> v0.15.5`: updated defaults for UserPoolWithMfa will break deployments for already created userPools. If migrating to a newer version of this package, be sure to set the following in your `cdk.context.json`:
+>
+>  
+>   ```
+>     {
+>       "@aws-prototyping-sdk/identity:useLegacyMFAProps":true
+>     }
+>   ```
+
 This module by default deploys a configurable Identity Provider with a default Cognito User Pool. These resources can be used by your website to restrict access to only authenticated users if needed. All settings are configurable and the creation of these AuthN resources can be disabled if needed or configured to use custom AuthN providers i.e. Facebook, Google, etc.
 
 Below is a conceptual view of the default architecture this module creates:
--- packages/identity/test/user-identity.test.ts
+++ packages/identity/test/user-identity.test.ts
@@ -4,12 +4,19 @@ import { PDKNag } from "@aws-prototyping-sdk/pdk-nag";
 import { App, NestedStack, Stack } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import { UserPool } from "aws-cdk-lib/aws-cognito";
-import { UserIdentity } from "../src";
+import { USE_LEGACY_MFA_PROPS_CONTEXT_KEY, UserIdentity } from "../src";
 
 describe("User Identity Unit Tests", () => {
   it("Defaults", () => {
     const stack = new Stack(PDKNag.app());
     new UserIdentity(stack, "Defaults");
+    expect(Template.fromStack(stack)).toMatchSnapshot();
+  });
+
+  it("Defaults - using legacy props", () => {
+    const stack = new Stack(PDKNag.app());
+    stack.node.setContext(USE_LEGACY_MFA_PROPS_CONTEXT_KEY, true);
+    new UserIdentity(stack, "Defaults Legacy Props");
     expect(Template.fromStack(stack)).toMatchSnapshot();
   });
 
