958a53c307824a22cf0c1b6ef021e74d04172399
Adrian Dimech <51220968+agdimech@users.noreply.github.com>
2023-05-30 12:49:11 +1000

    fix: revert all commits from 5895cb9fdf (#443)

--- private/projects/pdk-monorepo-project.ts
+++ private/projects/pdk-monorepo-project.ts
@@ -7,7 +7,6 @@ import {
   NxMonorepoProject,
   DEFAULT_CONFIG,
 } from "../../packages/nx-monorepo/src";
-import { NxRelease } from "../../packages/nx-monorepo/src/components/release";
 
 const HEADER_RULE = {
   "header/header": [
@@ -126,7 +125,7 @@ export class PDKMonorepoProject extends NxMonorepoProject {
       ".yarn/cache",
       ".yarn/__virtual__",
       ".pnp.cjs",
-      ".pnp.loader.cjs",
+      ".pnp.loader.cjs"
     );
 
     // add to local `.npmrc` to automatically avoid build hangs if npx is prompting to install a package
@@ -144,10 +143,6 @@ export class PDKMonorepoProject extends NxMonorepoProject {
     this.addTask("dev:workspace:unlink", {
       exec: "./scripts/dev/link-workspace.js unlink",
     });
-
-    const nxRelease = NxRelease.of(this) || new NxRelease(this);
-    // Exclude samples because they have explicit relative paths to 0.0.0 version artifacts for local development
-    nxRelease.nxArgs = ["--exclude='tag:sample'"];
   }
 
   /**
