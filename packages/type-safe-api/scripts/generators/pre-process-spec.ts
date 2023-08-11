18487d87ab66c9535bd4ef25ac4716b6089119d9
🚀 Jack <jacsteve@amazon.com>
2023-08-11 16:20:06 +1000

    feat(type-safe-api): generate infinite query hooks for smithy operations with paginated trait
    Instead of through the "jsonAdd" escape hatch, we now support using the @paginated trait in
    Smithy to generate useInfiniteQuery hooks in the TypeScript React Hooks library. The escape
    hatch will still work, since we still support specifying "x-paginated" for the OpenAPI use
    case. We achieve this by reading traits from the Smithy JSON model generated as part of the
    openapi projection (the model build task), and injecting them into the OpenAPI spec prior
    to code generation.

--- packages/type-safe-api/scripts/generators/pre-process-spec.ts
+++ packages/type-safe-api/scripts/generators/pre-process-spec.ts
@@ -5,11 +5,23 @@ import * as path from "path";
 import SwaggerParser from "@apidevtools/swagger-parser";
 import { parse } from "ts-command-line-args";
 
+// Smithy HTTP trait is used to map Smithy operations to their location in the spec
+const SMITHY_HTTP_TRAIT_ID = "smithy.api#http";
+
+// Maps traits to specific vendor extensions which we also support specifying in OpenAPI
+const TRAIT_TO_SUPPORTED_OPENAPI_VENDOR_EXTENSION: { [key: string]: string } = {
+  "smithy.api#paginated": "x-paginated",
+};
+
 interface Arguments {
   /**
    * Path to the spec to preprocess
    */
   readonly specPath: string;
+  /**
+   * Path to the smithy json model
+   */
+  readonly smithyJsonPath?: string;
   /**
    * JSON string containing extra vendor extensions to add to the spec
    */
@@ -20,14 +32,58 @@ interface Arguments {
   readonly outputSpecPath: string;
 }
 
+interface SmithyOperationDetails {
+  readonly id: string;
+  readonly method: string;
+  readonly path: string;
+  readonly traits: { [key: string]: any };
+}
+
 void (async () => {
   const args = parse<Arguments>({
     specPath: { type: String },
+    smithyJsonPath: { type: String, optional: true },
     extraVendorExtensions: { type: String, optional: true },
     outputSpecPath: { type: String },
   });
 
-  const spec = await SwaggerParser.bundle(args.specPath);
+  const spec = (await SwaggerParser.bundle(args.specPath)) as any;
+
+  if (args.smithyJsonPath) {
+    // Read the operations out of the Smithy model
+    const smithyModel = JSON.parse(
+      fs.readFileSync(args.smithyJsonPath, "utf-8")
+    );
+    const operations: SmithyOperationDetails[] = Object.entries(
+      smithyModel.shapes
+    )
+      .filter(
+        ([, shape]: [string, any]) =>
+          shape.type === "operation" &&
+          shape.traits &&
+          SMITHY_HTTP_TRAIT_ID in shape.traits
+      )
+      .map(([id, shape]: [string, any]) => ({
+        id,
+        method: shape.traits[SMITHY_HTTP_TRAIT_ID].method?.toLowerCase(),
+        path: shape.traits[SMITHY_HTTP_TRAIT_ID].uri,
+        traits: shape.traits,
+      }));
+
+    // Apply all operation-level traits as vendor extensions to the relevant operation in the spec
+    operations.forEach((operation) => {
+      if (spec.paths?.[operation.path]?.[operation.method]) {
+        Object.entries(operation.traits).forEach(([traitId, value]) => {
+          // By default, we use x-<fully_qualified_trait_id> for the vendor extension, but for extensions we support
+          // directly from OpenAPI we apply a mapping (rather than repeat ourselves in the mustache templates).
+          const vendorExtension =
+            TRAIT_TO_SUPPORTED_OPENAPI_VENDOR_EXTENSION[traitId] ??
+            `x-${traitId}`;
+          spec.paths[operation.path][operation.method][vendorExtension] = value;
+        });
+      }
+    });
+  }
 
   const processedSpec = {
     ...spec,
