26d462103b059075f003ede6580abb9028e6173f
Jack Stevenson <jacsteve@amazon.com>
2023-09-08 10:42:44 +1000

    fix(type-safe-api): ensure lambda permission policy size does not exceed limit for router (#563)
    When using the handler router (a single lambda function for all operations), type safe api would
    still create a new permission to grant api gateway permissions to invoke the lambda for each
    individual operation. With this change, we retain the granular permissions for individual lambdas,
    but as soon as a lambda function is used for multiple operations, we replace the granular permission
    with a broader permission for api gateway to invoke it for any operation, so as not to exceed the
    maximum policy size.
    
    fix #549

--- packages/type-safe-api/src/construct/integrations/lambda.ts
+++ packages/type-safe-api/src/construct/integrations/lambda.ts
@@ -33,6 +33,10 @@ export class LambdaIntegration extends Integration {
     };
   }
 
+  private getOperationPermissionId(operationId: string) {
+    return `LambdaPermission-${operationId}`;
+  }
+
   /**
    * Grant API Gateway permissions to invoke the lambda
    */
@@ -42,21 +46,65 @@ export class LambdaIntegration extends Integration {
     operationId,
     method,
     path,
+    operationLookup,
   }: IntegrationGrantProps) {
-    new CfnPermission(scope, `LambdaPermission-${operationId}`, {
-      action: "lambda:InvokeFunction",
-      principal: "apigateway.amazonaws.com",
-      functionName: this.lambdaFunction.functionArn,
-      sourceArn: Stack.of(scope).formatArn({
-        service: "execute-api",
-        resource: api.restApiId,
-        // Scope permissions to any stage and a specific method and path of the operation.
-        // Path parameters (eg {param} are replaced with wildcards)
-        resourceName: `*/${method.toUpperCase()}${path.replace(
-          /{[^\}]*\}/g,
-          "*"
-        )}`,
-      }),
-    });
+    // Router permissions are unique to a function
+    const routerPermissionId = `LambdaRouterPermission-${this.lambdaFunction.node.addr.slice(
+      -8
+    )}`;
+
+    // Check if we've already granted a router permission for this lambda
+    if (scope.node.tryFindChild(routerPermissionId)) {
+      return; // The function already has access to all operations
+    }
+
+    // Check if a permission has been added for other operations for the same function arn
+    const otherOperationPermissions = Object.keys(operationLookup)
+      .map((opId) =>
+        scope.node.tryFindChild(this.getOperationPermissionId(opId))
+      )
+      .filter(
+        (permission) =>
+          permission &&
+          permission instanceof CfnPermission &&
+          permission.functionName === this.lambdaFunction.functionArn
+      ) as CfnPermission[];
+
+    if (otherOperationPermissions.length > 0) {
+      // This lambda function is reused, so we add the "router permission" which allows
+      // invocation for any operation, to save exceeding the policy size limit for large
+      // numbers of operations.
+      otherOperationPermissions.forEach((permission) =>
+        scope.node.tryRemoveChild(permission.node.id)
+      );
+      new CfnPermission(scope, routerPermissionId, {
+        action: "lambda:InvokeFunction",
+        principal: "apigateway.amazonaws.com",
+        functionName: this.lambdaFunction.functionArn,
+        sourceArn: Stack.of(scope).formatArn({
+          service: "execute-api",
+          resource: api.restApiId,
+          // Permissions for all
+          resourceName: "*/*/*",
+        }),
+      });
+    } else {
+      // Add an individual operation permission since this lambda is not reused for multiple operations
+      new CfnPermission(scope, this.getOperationPermissionId(operationId), {
+        action: "lambda:InvokeFunction",
+        principal: "apigateway.amazonaws.com",
+        functionName: this.lambdaFunction.functionArn,
+        sourceArn: Stack.of(scope).formatArn({
+          service: "execute-api",
+          resource: api.restApiId,
+          // Scope permissions to any stage and a specific method and path of the operation.
+          // Path parameters (eg {param} are replaced with wildcards)
+          resourceName: `*/${method.toUpperCase()}${path.replace(
+            /{[^\}]*\}/g,
+            "*"
+          )}`,
+        }),
+      });
+    }
   }
 }
